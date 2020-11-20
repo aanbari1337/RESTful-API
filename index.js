84d2304cc668160e3c57b8e8e007eb6998b508c8
Amine Anbari <aanbari@e1r7p11.1337.ma>
2020-11-20 14:53:44 +0100

    error handling

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ const auth = require("./routes/auth");
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const config = require("config");
+const error = require("./middleware/error");
 
 const app = express();
-if(!config.get('jwtPrivateKey')){
-  console.log('FATAL ERROR')
-  process.exit(1)
+if (!config.get("jwtPrivateKey")) {
+  console.log("FATAL ERROR");
+  process.exit(1);
 }
 Joi.objectId = require("joi-objectid")(Joi);
 
 app.use(express.json());
 mongoose
-  .connect("mongodb+srv://aanbari:MJAocyUpUidaPiIR@restfulapi.b0q7e.mongodb.net/DbRestfulApi?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://aanbari:MJAocyUpUidaPiIR@restfulapi.b0q7e.mongodb.net/DbRestfulApi?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("connected to mongodb"))
   .catch(() => console.log("couldn't connect"));
 
@@ -31,6 +35,7 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
